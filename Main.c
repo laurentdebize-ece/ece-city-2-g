c764e9df832a1891155eba2235f7d9bbd888bbf6
emilien <emilien.godet@edu.ece.fr>
2022-11-08 14:46:30 +0100

    version avec bibliothèques nécessaires

--- Main.c
+++ Main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "sims.h"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <time.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 int main()
 {
---
+++ sims.h
@@ -0,0 +1,4 @@
+#ifndef ECE_CITY_2_G_SIMS_H
+#define ECE_CITY_2_G_SIMS_H
+
+#endif //ECE_CITY_2_G_SIMS_H
