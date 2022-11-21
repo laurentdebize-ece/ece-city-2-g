616405100d4dcf784c39959bf471b2c892fa4fe8
luciebo <115739837+luciebo@users.noreply.github.com>
2022-11-21 15:29:34 +0100

    Add files via upload

---
+++ TP6_BAUD/main.c
@@ -0,0 +1,11 @@
+#include "Graphe.h"
+
+int main()
+{
+    Graphe* graphe= conversionTXTgraphe();
+    //Graphe * g=lire_graphe("tp6.txt");
+    //graphe_afficher(g);    // afficher le graphe
+    //g= AlimentationEnEau(g);
+    //AlimentationElectricite(g);
+    return 0;
+}
---
+++ TP6_BAUD/source.c
@@ -0,0 +1,6 @@
+//
+// Created by lulub on 10/11/2022.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
